ecaf34ac4ee55fee5d932b65f6c4dcb665f7c2a2
chidubemokeke <92036409+chidubemokeke@users.noreply.github.com>
2024-06-25 16:00:44 +0100

    update-config

--- frontend/my-dapp/queries/userQueries.js
+++ frontend/my-dapp/queries/userQueries.js
@@ -1,31 +1,32 @@
 import { gql } from "@apollo/client";
 
-export const GET_USER_POSTS_OVER_TIME = gql`
-  query GetUserPostsOverTime(
-    $userId: ID!
-    $startDate: BigInt!
-    $endDate: BigInt!
-  ) {
-    userPostsCreateds(
-      where: {
-        user: $userId
-        timestamp_gte: $startDate
-        timestamp_lt: $endDate
-      }
-    ) {
-      id
-      timestamp
-      content
-    }
-  }
-`;
-
 export const GET_QUERY_WITHDRAWALS_BY_USER = gql`
   query GetUserWithdrawals($userId: ID!) {
     etherWithdrawns(where: { owner: $userId }) {
       id
       amount
       timestamp
+    }
+  }
+`;
+
+export const GET_USER_CONNECTIONS = gql`
+  query GetUserConnections($userId: ID!) {
+    user(id: $userId) {
+      followers {
+        id
+        follower {
+          id
+          username
+        }
+      }
+      following {
+        id
+        followee {
+          id
+          username
+        }
+      }
     }
   }
 `;
