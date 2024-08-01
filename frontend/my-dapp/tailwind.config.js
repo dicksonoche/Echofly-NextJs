54c97605d774104b88daf3052ac2c7532e75d89f
Simon Dickson <simonoche987@gmail.com>
2024-08-01 14:09:00 +0100

    frontend setup

---
+++ frontend/my-dapp/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
