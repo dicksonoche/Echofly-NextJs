9e19be52e20b7667865d34a232a95075e7922c39
Simon Dickson <simonoche987@gmail.com>
2024-09-24 11:25:18 +0100

    fixes

--- frontend/my-dapp/src/lib/ethereum.js
+++ frontend/my-dapp/src/lib/ethereum.js
@@ -1,13 +1,13 @@
 import { ethers } from "ethers";
 
-let provider;
+let provider: ethers.providers.Web3Provider | ethers.providers.JsonRpcProvider;
 
 if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-  // We are in the browser and metamask is running.
-  window.ethereum.request({ method: "eth_requestAccounts" }); // Request access to account
+  // We are in the browser and MetaMask is available.
+  window.ethereum.request({ method: "eth_requestAccounts" });
   provider = new ethers.providers.Web3Provider(window.ethereum);
 } else {
-  // We are on the server *OR* the user is not running metamask
+  // We are either on the server or the user doesn't have MetaMask installed.
   provider = new ethers.providers.JsonRpcProvider(
     "https://sepolia.infura.io/v3/45659e58bfd842309ac5e26ecd083106"
   );
