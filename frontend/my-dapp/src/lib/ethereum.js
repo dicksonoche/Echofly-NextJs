c8dff7525ea2c49634871b16e900f4163663ea31
Agboola Olamidipupo <58988405+Olamidipupo-favour@users.noreply.github.com>
2024-10-04 11:12:17 +0100

    update: exporting key bars from Ethereum.js
    Signed-off-by: Agboola Olamidipupo  <58988405+Olamidipupo-favour@users.noreply.github.com>

--- frontend/my-dapp/src/lib/ethereum.js
+++ frontend/my-dapp/src/lib/ethereum.js
@@ -12,5 +12,9 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
     "https://sepolia.infura.io/v3/45659e58bfd842309ac5e26ecd083106"
   );
 }
-// export a signer
+const contractAddress="";
+const contractABI=[];
+export const contract = new ethers.Contract(contractAddress, contractABI, provider);
+export const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+export const contractWithSigner = contract.connect(signer);
 export  provider;
